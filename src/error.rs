b211d958079e55dedc90391162f8f0952988cf27
Szymon Walter <walter.szymon.98@gmail.com>
2018-03-18 18:34:26 +0100

    replace Error/ErroKind pair with just `enum Error`

--- src/error.rs
+++ src/error.rs
@@ -1,22 +1,7 @@
-/// Wrapper around the raw `ErrorKind`
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub struct Error {
-    kind: ErrorKind,
-}
-
-impl Error {
-    pub fn from_kind(kind: ErrorKind) -> Error {
-        Error { kind }
-    }
-
-    pub fn kind(&self) -> ErrorKind {
-        self.kind
-    }
-}
-
 /// The set of all possible errors
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-pub enum ErrorKind {
-    BadMagic,
-    OutOfBounds,
+pub enum Error {
+    BadMagic(u16),
+    OutOfBounds(usize),
+    BadBlockGroupCount(u32, u32),
 }
